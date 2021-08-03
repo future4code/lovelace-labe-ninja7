d5822b2a16fd2bbd095df4053d5c04a86fd24994
GlaytonVerissimo <glayton.verissimo@hotmail.com>
2021-08-03 17:20:27 -0300

    Home e cadastro criados, falta arrumar as funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import Home from './components/Home'
+import Cadastro from './components/Cadastro'
 
-function App() {
-	return (
-        <AppContainer />
-	)
+
+export default class App extends React.Component{
+	render(){
+		return(
+			<div>
+			
+				
+			</div>
+		)
+	}
 }
-
-export default App
---
+++ src/components/Cadastro.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CentralizarCadastro = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+    text-align: center
+`
+
+
+export default class Cadastro extends React.Component{
+    render(){
+        return(
+            <CentralizarCadastro>
+            <div>
+
+                <h1>Cadastre o seu serviço</h1>
+
+            
+            <input  type="text" placeholder="Titulo" />
+            <br/><br/>
+            <input type="text" placeholder="Descição"/>
+            <br/><br/>
+            <input type="text" placeholder="Preço"/>
+            <br/><br/>
+           
+            
+            <select name="info[]" size="5" multiple>
+            <option value="">Cartão de Débito</option>
+            <option value="">Cartão de Crédito</option>
+            <option value="">PayPal</option>
+            <option value="">Boleto</option>
+            <option value="">Pix</option>
+            </select>
+            <br/><br/>
+            
+            
+            <input type="date"/>
+            <br/><br/>
+            <button>Cadastrar serviço</button>
+            
+            </div>
+            </CentralizarCadastro>
+          
+        )
+    }
+}
